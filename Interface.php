23917d90b0faf34b22727d1206fde017fa25ec49
Labros Kittas <kittaslabros@gmail.com>
2018-12-21 11:07:15 +0200

    Nasa Rover test

---
+++ Interface.php
@@ -0,0 +1,73 @@
+<?php
+
+	echo "Welcome to the NASA Rover console command line!\xA";
+	$input = readline("Please start by choosing input from either commands or file(type <command> or <file>): ");
+
+	while (empty($input)
+		|| ($input !== "command" && $input !== "file")
+	) {
+		$input = readline("Please choose corrent input from either commands or file(type <command> or <file>): ");
+	}
+
+	if ($input === "command") {
+		
+		//TODO: implement read from command line.
+		echo "CLI not yet implemented... Continuing with read from file.\xA";
+
+	}
+
+	try {
+
+		$filepath = readline("Please specify path to text file with the instructions:");
+
+		if (empty($filepath)) {
+			throw new Exception("Empty filepath!");
+		}
+
+		$file = file("./{$filepath}");
+		$moveset = "";
+		$rovers = array();
+
+		foreach ($file as $line => $contents) {
+			if ($line === 0) {
+				$bounds = explode(" ", $contents);
+			} elseif ($line % 2 === 0) {
+				$moveset = $contents;
+			} else {
+				$coordinates = explode(" ", $contents);
+			}
+
+			require_once './Rover.php';
+
+			if (!empty($moveset)) {
+
+				$rover_to_move = new Rover($coordinates, $bounds[0], $bounds[1]);
+
+				$moves = str_split($moveset);
+				
+				foreach ($moves as $index => $move) {
+					if (!empty($move)) {
+						$rover_to_move->move($move);
+					}
+				}
+
+				$moveset = "";
+				$rovers[] = $rover_to_move;
+
+			}
+
+		}
+
+		foreach ($rovers as $index => $rover) {
+			$print_out = implode(" ", $rover->get_coordinates());
+			echo "{$print_out}\xA";
+		}
+		
+	} catch (Exception $e) {
+		
+		echo $e->getMessage() . "\xA";
+		echo "Created on line: {$e->getLine()} \xA";
+
+	}
+
+?>
---
+++ Rover.php
@@ -0,0 +1,153 @@
+<?php
+
+	/**
+	*
+	*/
+	class Rover {
+
+		const COMPASS = array("N", "E", "S", "W");
+		var $coordinates = array();
+		var $height_bound;
+		var $width_bound;
+
+		public function __construct($coordinates=array(), $height_bound=0, $width_bound=0) {
+
+			$this->coordinates = $coordinates;
+			$this->height_bound = $height_bound;
+			$this->width_bound = $width_bound;
+
+		}
+
+		public function get_coordinates() {
+
+			return $this->coordinates;
+
+		}
+
+		private function set_coordinates($coordinates=array()) {
+
+			$this->coordinates = $coordinates;
+
+		}
+
+		/**
+		* 
+		*/
+		public function move($movement="") {
+
+			switch($movement) {
+				case "L":
+
+				case "R":
+					$this->strafe($movement);
+					break;
+
+				case "M":
+					$this->take_a_step();
+					break;
+
+				default:
+					//echo "Movement {$movement} is not a valid moveset. Please try another.\xA";
+					break;
+			}
+
+		}
+
+		/**
+		*
+		*/
+		private function strafe($direction="") {
+
+			$current_index = array_search($this->coordinates[2], self::COMPASS);
+
+			$new_index = -1;
+
+			if ($direction === "L") {
+				if ($current_index === 3) {
+
+					$new_index = 0;
+
+				} else {
+
+					$new_index = $current_index++;
+
+				}
+
+			} elseif ($direction === "R") {
+				
+				if ($current_index === 0) {
+					
+					$new_index = 3;
+
+				} else {
+
+					$new_index = $current_index--;
+
+				}
+
+			}
+
+			$this->coordinates[2] = self::COMPASS[$new_index];
+
+		}
+
+		/**
+		*
+		*/
+		private function take_a_step() {
+
+			$new_coordinates = $this->coordinates;
+			
+			switch ($new_coordinates[2]) {
+				case "N":
+					$new_coordinates[1]++;
+					break;
+
+				case "E":
+					$new_coordinates[0]++;
+					break;
+
+				case "S":
+					$new_coordinates[1]--;
+					break;
+
+				case "W":
+					$new_coordinates[0]--;
+					break;
+				
+				default:
+					break;
+			}
+
+			$this->check_boundaries($new_coordinates);
+
+		}
+
+		/**
+		*
+		*/
+		private function check_boundaries($new_coordinates=array()) {
+
+			foreach ($new_coordinates as $index => $value) {
+				
+				if (is_numeric($value)) {
+					
+					if ($value > $this->width_bound) {
+						$new_coordinates[$index] = $this->width_bound;
+					} elseif ($value > $this->height_bound) {
+						$new_coordinates[$index] = $this->height_bound;
+					} elseif ($value < 0) {
+						$new_coordinates[$index] = 0;
+					}
+
+				}
+
+			}
+
+			$this->set_coordinates($new_coordinates);
+
+		}
+
+	}
+
+?>
